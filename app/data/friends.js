efa2c643c339ea71ff6cde23e1f980207caf5e43
Shayda Bakhshi <shayda.bakhshi@gmail.com>
2020-05-06 23:58:43 -0500

    added basic JS functionality in survey.html; populated friendsArray

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,50 @@
 // Create a "friends" array with which to match users
 var friendsArray = [{
-    name: "Elizabeth",
-    photo: "https://avangelista.files.wordpress.com/2013/04/the-pelican-portrait.jpg",
-    scores: [4, 5, 3, 2, 1, 4, 5, 3, 4, 4]
-}];
+        name: "Cassandra",
+        photo: "https://i.pinimg.com/236x/54/0d/e1/540de1b2a98452d690d1409b6a0ba187.jpg",
+        scores: [4, 5, 3, 2, 1, 4, 5, 3, 4, 4]
+    },
+    {
+        name: "Varric",
+        photo: "https://i.pinimg.com/236x/61/6c/dd/616cddef96818aec9f3b7e8e650e80d7.jpg",
+        scores: [2, 5, 2, 1, 1, 5, 5, 2, 2, 1]
+    },
+    {
+        name: "Aveline",
+        photo: "https://i.pinimg.com/564x/d9/52/7b/d9527b9cea8845164d840b02e10ac984.jpg",
+        scores: [3, 4, 2, 2, 2, 3, 4, 1, 5, 1]
+    },
+    {
+        name: "Hawke",
+        photo: "https://i.pinimg.com/236x/b5/2d/5c/b52d5c46f089933cc25754ed8f30d9e0.jpg",
+        scores: [2, 1, 5, 5, 5, 3, 2, 1, 3, 4]
+    },
+    {
+        name: "Dorian",
+        photo: "https://i.pinimg.com/236x/29/58/40/295840ff4a19e94254c84f20516f73bf.jpg",
+        scores: [2, 4, 2, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+        name: "Liara",
+        photo: "https://i.pinimg.com/236x/6d/5c/9f/6d5c9fb123cb6a882d5cd6e50aa93def.jpg",
+        scores: [2, 1, 2, 5, 2, 5, 5, 2, 2, 5]
+    },
+    {
+        name: "Shepard",
+        photo: "https://i.pinimg.com/236x/a4/a8/2c/a4a82c3afd68a0c26c7c3d3f08fa38b2.jpg",
+        scores: [2, 5, 2, 3, 4, 5, 5, 2, 2, 2]
+    },
+    {
+        name: "Garrus",
+        photo: "https://i.pinimg.com/236x/b6/9c/d1/b69cd1442ff3d03f63dbb8c202a1b7e1.jpg",
+        scores: [3, 3, 3, 4, 5, 5, 1, 1, 1, 2]
+    },
+    {
+        name: "Tali",
+        photo: "https://i.pinimg.com/236x/f4/53/48/f453484647da84a780d073f477b7eca5.jpg",
+        scores: [4, 5, 2, 2, 4, 2, 2, 2, 2, 1]
+    }
+];
 
 // Export the array so we can access it in other files:
 module.exports = friendsArray;
