162135802c3a3cb95f15b20a8e3aeafb7bfd193f
Shayda Bakhshi <shayda.bakhshi@gmail.com>
2020-05-29 23:58:46 -0500

    finalized friend match functionality on back end

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,37 +4,50 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-
 var friendsArray = require("../data/friends");
-
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
+module.exports = function (app) {
+  // Your `apiRoutes.js` file should contain two routes:
 
-module.exports = function(app) {
+  // A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
 
+  app.get("/api/friends", function (req, res) {
+    res.json(friendsArray);
+  });
 
-    // Your `apiRoutes.js` file should contain two routes:
+  // A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
-    // A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
+  app.post("/api/friends", function (req, res) {
+    var newFriend = req.body;
+    var newFriendScores = newFriend.scores;
+    var scoresArray = [];
+    var bestMatch = 0;
 
-    app.get("/api/friends", function(req, res) {
-        res.json(friendsArray);
-    });
+    for (var i = 0; i < friendsArray.length; i++) {
+      var scoreDiff = 0;
 
-    // A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+      for (var j = 0; j < newFriendScores.length; j++) {
+        scoreDiff += Math.abs(
+          parseInt(friendsArray[i].scores[j]) - parseInt(newFriendScores[j])
+        );
+      }
+      scoresArray.push(scoreDiff);
+    }
+    for (var i = 0; i < scoresArray.length; i++) {
+      if (scoresArray[i] <= scoresArray[bestMatch]) {
+        bestMatch = i;
+      }
+    }
 
-    app.post("/api/friends", function(req, res) {
-        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-        // It will do this by sending out the value "true" have a table
-        // req.body is available since we're using the body parsing middleware
+    var besties = friendsArray[bestMatch];
+    res.json(besties);
 
-        friendsArray.push(req.body);
-        res.json(true);
+    console.log(besties);
 
-    });
-
-
-}
+    friendsArray.push(newFriend);
+  });
+};
