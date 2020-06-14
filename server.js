360b68d372f4d381eb5b57a6211049fabf37f704
Shayda Bakhshi <shayda.bakhshi@gmail.com>
2020-06-14 16:04:30 -0500

    updated server.js to include heroku port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -26,6 +26,6 @@ require("./app/routing/htmlRoutes")(app);
 // LISTENER
 // =============================================================================
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
 });
